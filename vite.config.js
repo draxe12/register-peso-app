d5ce3bdb802c939d19097a7b987a42f26fe7da09
Junior Mejia Osorio <jmejiaosorio.1296@gmail.com>
2025-10-10 12:12:42 -0500

    added imgs y pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['logo.svg', 'pwa-64x64.png', 'pwa-192x192.png', 'pwa-512x512.png', 'maskable-icon-512x512.png'], // Ajusta tus iconos
+      manifest: {
+        name: 'Registro de Pesos de Pollos',
+        short_name: 'RegisterPeso',
+        description: 'Registro de Pesos de Pollos para obtener promedio y uniformidad',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            "src": "/pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+          },
+          {
+            "src": "/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "/maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      }
+    })
+  ],
   base: '/register-peso-app/'
 })

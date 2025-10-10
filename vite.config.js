26c60941c88e5cb39cc485e1cd7dbd57ec694591
Junior Mejia Osorio <jmejiaosorio.1296@gmail.com>
2025-10-10 12:59:44 -0500

    agregado el base a src images

--- vite.config.js
+++ vite.config.js
@@ -21,22 +21,22 @@ export default defineConfig({
         lang: 'es',
         icons: [
           {
-            "src": "/pwa-64x64.png",
+            "src": "/register-peso-app/pwa-64x64.png",
             "sizes": "64x64",
             "type": "image/png"
           },
           {
-            "src": "/pwa-192x192.png",
+            "src": "/register-peso-app/pwa-192x192.png",
             "sizes": "192x192",
             "type": "image/png"
           },
           {
-            "src": "/pwa-512x512.png",
+            "src": "/register-peso-app/pwa-512x512.png",
             "sizes": "512x512",
             "type": "image/png"
           },
           {
-            "src": "/maskable-icon-512x512.png",
+            "src": "/register-peso-app/maskable-icon-512x512.png",
             "sizes": "512x512",
             "type": "image/png",
             "purpose": "maskable"
